56b0422e746086e0244df094bc5e7cf79c44cfee
zen-dell <ngdatuananh@gmail.com>
2022-02-04 11:34:58 +0700

    Updates

---
+++ static/js/24.ca8fb2dd.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@minimal/minimal-kit-react"]=this["webpackJsonp@minimal/minimal-kit-react"]||[]).push([[24],{1127:function(i,a,t){"use strict";t.r(a);var m=t(1360);a.default=m.a}}]);
+//# sourceMappingURL=24.ca8fb2dd.chunk.js.map
